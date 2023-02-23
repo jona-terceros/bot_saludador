3818f9607832330afaa63aba8be00fc4cdf9ad91
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-02-22 22:08:54 -0400

    Saludar por edad

--- src/presenter.js
+++ src/presenter.js
@@ -5,26 +5,39 @@ const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar");
 const sexo = document.querySelector("#box")
 const div = document.querySelector("#saludador");
-
+const edad = document.querySelector("#edad")
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   //const username = nombre.value;
-  //div.innerHTML = "<p> Hola " + username + "</p>";
+ 
 
+  //div.innerHTML = "<p> Hola " + username + "</p>";
   let saludo = "";
   if (sexo.value == "masculino")
   {
-
-    div.innerHTML = saludo + " señor " +  nombre.value;
-
+    if (edad.value >30)
+    {
+      div.innerHTML = saludo + " señor " +  nombre.value;
+    }
+    else
+    {
+      div.innerHTML = saludo + " joven " +  nombre.value;
+    }
   }
   else{
-
-    div.innerHTML = saludo + " señora " +  nombre.value;
-   
+    if (edad.value>30)
+    {
+      div.innerHTML = saludo + " señora " +  nombre.value;
+    }
+    else
+    {
+      div.innerHTML = saludo + "señorita " +  nombre.value;
+    }
   }
 });
+
+
 
