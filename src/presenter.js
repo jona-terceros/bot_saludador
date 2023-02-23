3104c041e21e2d9c46bcac40fc17ef30e1d2ebf5
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-02-22 21:49:42 -0400

    Saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,18 +1,30 @@
-import sumar from "./sumador";
+  import sumar from "./sumador";
 
 
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar");
-const div = document.querySelector("#resultado-div");
+const sexo = document.querySelector("#box")
+const div = document.querySelector("#saludador");
+
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const username = nombre.value;
- 
+  //const username = nombre.value;
+  //div.innerHTML = "<p> Hola " + username + "</p>";
 
-  div.innerHTML = "<p> Hola " + username + "</p>";
+  let saludo = "";
+  if (sexo.value == "masculino")
+  {
 
+    div.innerHTML = saludo + " señor " +  nombre.value;
 
+  }
+  else{
+
+    div.innerHTML = saludo + " señora " +  nombre.value;
+   
+  }
 });
 
